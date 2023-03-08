d35872cad046523457049f2a5ce6ee2605f2d0e5
Varun Kadkade <varunkadkade2442@gmail.com>
2023-03-08 13:22:05 +0530

    add pading to navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   const fullName = `${user.firstName} ${user.lastName}`;
 
   return (
-    <FlexBetween padding="1rem" backgroundColor={alt}>
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
       <FlexBetween gap="1.75rem">
         <Typography
           fontWeight="bold"
